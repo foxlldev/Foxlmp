c8da6fbaa29d4594ca5d5d4310cf96cd1a13fff3
foxl <every.flipped@gmail.com>
2022-08-09 15:57:07 +0100

    e

--- ui/assets/script.js
+++ ui/assets/script.js
@@ -8,37 +8,150 @@ let gaudio = null;
 let selectFileButton = document.getElementById('selectFileButton');
 let playButton = document.getElementById('playButton');
 let setVolButton = document.getElementById('setVolButton');
-let loopToggle = document.getElementById('looptgl');
+let loopToggle = document.getElementById('loopButton').children[0];
 
 var latestvolume = 1;
 
-function updateTime() {
-    var current = new Date(null);
-    current.setSeconds(gaudio.currentTime);
-    var duration = new Date(null);
-    duration.setSeconds(gaudio.duration);
-    //document.getElementById('timelbl').innerText = Math.round(gaudio.currentTime/60*100)/100+"/"+Math.round(gaudio.duration/60*100)/100;
-    document.getElementById('timelbl').innerText = current.toISOString().substr(14,5)+" / "+duration.toISOString().substr(14,5);
+let queue = [];
+let queuePos = 0;
 
-    if (current >= duration && loopToggle.checked) {
-        gaudio.play();
+let dragging = false;
+
+function toggleLoop() {
+    // repeat song > repeat list
+    if (loopToggle.classList.contains('fa-repeat')) {
+        window.localStorage.setItem('loop', 'repeatlist');
+        loopToggle.classList.replace('fa-repeat', 'fa-arrow-down-short-wide');
     }
+    // repeat list > stop
+    else if (loopToggle.classList.contains('fa-arrow-down-short-wide')) {
+        window.localStorage.setItem('loop', 'stop');
+        loopToggle.classList.replace('fa-arrow-down-short-wide', 'fa-stopwatch');
+    }
+    // stop after song > repeat song
+    else if (loopToggle.classList.contains('fa-stopwatch')) {
+        window.localStorage.setItem('loop', 'repeatsong');
+        loopToggle.classList.replace('fa-stopwatch', 'fa-repeat');
+    } else {
+        console.log("error");
+    }
+}
+
+function loadLoop() {
+    loopToggle.classList.remove('fa-repeat');
+    loopToggle.classList.remove('fa-arrow-down-short-wide');
+    loopToggle.classList.remove('fa-stopwatch');
+
+    if (window.localStorage.getItem('loop') === 'repeatsong') loopToggle.classList.add('fa-repeat');
+    else if (window.localStorage.getItem('loop') === 'repeatlist') loopToggle.classList.add('fa-arrow-down-short-wide');
+    else if (window.localStorage.getItem('loop') === 'stop') loopToggle.classList.add('fa-stopwatch');
+    else console.log("error");
+}
+
+function updateTime() {
+    let current = new Date(null);
+    current.setSeconds(gaudio.currentTime);
+    let duration = new Date(null);
+    duration.setSeconds(gaudio.duration);
+    document.getElementById('point-current').innerText = current.toISOString().substring(19,14);
+    document.getElementById('point-duration').innerText = duration.toISOString().substring(19,14);
+
+    if (dragging === false) {
+        document.getElementById('point-slider').max = gaudio.duration;
+        document.getElementById('point-slider').value = gaudio.currentTime;
+    }
+
+    if (current >= duration) {
+        if (loopToggle.classList.contains('fa-repeat')) {
+            gaudio.play();
+        } else if (loopToggle.classList.contains('fa-arrow-down-short-wide') && queue.length > 0) {
+            gaudio = queue[queuePos+1].audio;
+            loadDetails(queue[queuePos+1].fpz);
+            console.log("done apparently")
+            loadDetails(queue[queuePos+1].fpz);
+            gaudio.play();
+            let newQueue = [];
+            for (let i = 0; i < queue.length; i++) {
+                if ((queuePos+1) <= i) newQueue.push(queue[i]);
+            }
+            queue = newQueue;
+            queuePos = queue.indexOf(gaudio);
+        }
+        updateQueueWindow();
+    }
+}
+
+function setProgress() {
+    let slider = document.getElementById('point-slider');
+
+    gaudio.currentTime = slider.value;
+}
+
+function setVolume() {
+    let val = document.getElementById('volume-slider').value;
+    gaudio.volume = val/100;
+    document.getElementById('volume-label').innerText = val+"%";
 }
 
 playButton.onclick = async () => {
     if (gaudio != null) {
-        if (gaudio.paused){gaudio.play();playButton.innerText="Pause";}
-        else if (!gaudio.paused){gaudio.pause();playButton.innerText="Play";}
+        if (gaudio.paused){gaudio.play();playButton.innerHTML="<i class='fa-solid fa-pause'></i>";}
+        else if (!gaudio.paused){gaudio.pause();playButton.innerHTML="<i class='fa-solid fa-play'></i>";}
     }
 }
 
-setVolButton.onclick = async () => {
-    let volinpval = document.getElementById('volinp').value;
-    if (gaudio != null) {gaudio.volume = volinpval/100; console.log("new gaudio volume: "+gaudio.volume)}
-    latestvolume = volinpval;
+async function getDetails(fpz, callback) {
+    jsmt.read(fpz, {
+        onSuccess: (tag) => {
+            callback(tag);
+        },
+        onError: (error) => {
+            console.log(error);
+        }
+    });
 }
 
-selectFileButton.onclick = async () => {
+async function loadDetails(fpz) {
+    jsmt.read(fpz, {
+        onSuccess: (tag) => {
+            var image = tag.tags.picture;
+            if (image) {
+                var base64String = "";
+                for (var i = 0; i < image.data.length; i++) {
+                    base64String += String.fromCharCode(image.data[i]);
+                }
+                var base64 = "data:" + image.format + ";base64," + window.btoa(base64String);
+                document.getElementById('picture').setAttribute('src', base64);
+            } else {
+                document.getElementById('picture').setAttribute('src', "./assets/albumplaceholder.png");
+                console.log("no image lolol");
+            }
+            document.getElementById('details').style.display = "flex";
+            if (window.localStorage.getItem('loop') == "true") loopToggle.checked = "checked";
+            document.getElementById('selectFileButton').style.display = "none";
+            // Filename
+            try {var fn = fpz.split(/(\\|\/)/g).pop(); } catch(err) {console.log(err);}
+            console.log("file name: "+fn);
+            document.getElementById('into-filename').innerText = fn.toUpperCase();
+            // Artist - Title
+            let intotitle = document.getElementById('into-title');
+            let title = tag.tags.title !== undefined ? tag.tags.title : "Unknown";
+            let artist = tag.tags.artist !== undefined ? tag.tags.artist : "Unknown";
+            intotitle.innerText = artist + " - " + title;
+            // Album
+            let intoalbum = document.getElementById('into-album');
+            let album = tag.tags.album  !== undefined ? tag.tags.album : "Unknown album";
+            intoalbum.innerText = album;
+
+            console.log(tag);
+        },
+        onError: (error) => {
+            console.log(error);
+        }
+    });
+}
+
+async function getFileAndPlay() {
     let file = await dialog.showOpenDialog(mainWindow, {
         filters: [
             {
@@ -48,53 +161,62 @@ selectFileButton.onclick = async () => {
         ]
     });
 
-    if (file != undefined || file != null) {
+    if (file !== undefined && file !== null && file.canceled === false) {
         let audio = new Audio(file.filePaths[0]);
-        if (gaudio != null) { gaudio.pause(); gaudio.currentTime = 0; }
-        gaudio = new Audio(file.filePaths[0]);
+        let fpz = file.filePaths[0];
+        console.log(audio);
+        queue.push({audio, fpz});
+        if (gaudio !== null) { openQueueWindow(); return; }
+        console.log(gaudio)
+        gaudio = audio;
         gaudio.play();
+        console.log(gaudio)
 
-        gaudio.volume = latestvolume/100;
+        //gaudio.volume = latestvolume/100;
 
-        document.getElementById('playButton').style.display = "initial";
+        console.log(document.getElementsByClassName('onlyPlayingButtons'))
+        for (var i = 0; i < document.getElementsByClassName('onlyPlayingButtons').length; i++) {
+            document.getElementsByClassName('onlyPlayingButtons')[i].style.display = "grid";
+        }
+        loadLoop();
+        playButton.innerHTML = "<i class='fa-solid fa-pause'></i>";
 
-        document.getElementById('timelbl').innerText = "Loading...";
+        //document.getElementById('timelbl').innerText = "0:00";
+        document.getElementById('point-slider').max = gaudio.duration;
         setInterval(updateTime, 1000);
 
         try {var fn = file.filePaths[0].split(/(\\|\/)/g).pop(); } catch(err) {console.log(err);}
 
-        jsmt.read(file.filePaths[0], {
-            onSuccess: (tag) => {
-                var image = tag.tags.picture;
-                if (image) {
-                    var base64String = "";
-                    for (var i = 0; i < image.data.length; i++) {
-                        base64String += String.fromCharCode(image.data[i]);
-                    }
-                    var base64 = "data:" + image.format + ";base64," + window.btoa(base64String);
-                    document.getElementById('picture').setAttribute('src', base64);
-                } else {
-                    document.getElementById('picture').setAttribute('src', "./assets/albumplaceholder.png");
-                    console.log("no image lolol");
-                }
-                // Title
-                console.log("file name: "+fn);
-                if (tag.tags.title != undefined){document.getElementById('title').innerText = tag.tags.title+" ("+fn+")";}
-                else if (tag.tags.title == undefined){document.getElementById('title').innerText = "Unknown ("+fn+")";}
+        loadDetails(fpz);
+    }
+}
 
-                // Artist : Album
-                if (tag.tags.artist != undefined && tag.tags.album != undefined){document.getElementById('artistalbum').innerText = tag.tags.artist + " : " + tag.tags.album;}
-                else if (tag.tags.artist != undefined && tag.tags.album == undefined){document.getElementById('artistalbum').innerText = tag.tags.artist + " : " + "Unknown"}
-                else if (tag.tags.artist == undefined && tag.tags.album != undefined){document.getElementById('artistalbum').innerText = "Unknown" + " : " + tag.tags.album;}
-                else if (tag.tags.artist == undefined && tag.tags.album == undefined){document.getElementById('artistalbum').innerText = "Unknown";}
-                else {console.log("error");}
+function openQueueWindow() {
+    let queueWrapper = document.getElementById('queue');
+    queueWrapper.style.display = "block";
+    document.getElementById('content').style.marginRight = "20%";
+    document.getElementById('bottom').style.width = "calc(100% - 20% - 30px)";
 
-                console.log(tag);
-                playButton.innerText = "Pause";
-            },
-            onError: (error) => {
-                console.log(error);
-            }
+    updateQueueWindow();
+}
+
+function updateQueueWindow() {
+    let queueWrapper = document.getElementById('queue');
+
+    queueWrapper.innerHTML = " ";
+
+    for (let i = 0; i < queue.length; i++) {
+        console.log("it "+i)
+        let fn = queue[i].fpz.split(/(\\|\/)/g).pop();
+
+        getDetails(queue[i].fpz, async (tag) => {
+            console.log(tag);
+            console.log(queueWrapper.innerHTML)
+            queueWrapper.innerHTML +=
+            `<div class="queue-item">
+                <p class="queue-detail queue-name">${fn.toUpperCase()}</p>
+                <p class="queue-detail queue-details">${tag.tags.artist !== undefined ? tag.tags.artist : "Unknown artist"} - ${tag.tags.title !== undefined ? tag.tags.title : "Unknown title"}</p>
+            </div>`;
         });
     }
 }
